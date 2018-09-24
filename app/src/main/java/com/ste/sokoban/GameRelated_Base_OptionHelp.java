544626cfcdb63fcc320baad2b3708ddf29e7fb53
Ezio <assassinste@gmail.com>
2018-09-24 20:59:02 +0800

    Stage 2.3 - Add optionmenu - help

---
+++ app/src/main/java/com/ste/sokoban/GameRelated_Base_OptionHelp.java
@@ -0,0 +1,13 @@
+package com.ste.sokoban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameRelated_Base_OptionHelp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_related_base_option_help);
+    }
+}
--- app/src/main/java/com/ste/sokoban/GameRelated_Func_NewGame.java
+++ app/src/main/java/com/ste/sokoban/GameRelated_Func_NewGame.java
@@ -1,7 +1,11 @@
 package com.ste.sokoban;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GameRelated_Func_NewGame extends Activity {
 
@@ -9,5 +13,29 @@ public class GameRelated_Func_NewGame extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_related_func_new_game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflatr = new MenuInflater(this);
+        inflatr.inflate(R.menu.option_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemHelp:
+                Intent intentHelp = new Intent(
+                        GameRelated_Func_NewGame.this,
+                        GameRelated_Base_OptionHelp.class
+                );
+
+                startActivity(intentHelp);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
