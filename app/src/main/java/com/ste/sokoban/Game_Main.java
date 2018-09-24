0abaf1407b23290926e546c031b066925807ff9f
Ezio <assassinste@gmail.com>
2018-09-24 16:33:12 +0800

    Stage 0 - Init a project

---
+++ app/src/main/java/com/ste/sokoban/Game_Main.java
@@ -0,0 +1,13 @@
+package com.ste.sokoban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Game_Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_main);
+    }
+}
