a6c90d31f0e6579d9600b4b3a9ee5ac8e77a99b2
Ezio <assassinste@gmail.com>
2018-09-24 19:39:20 +0800

    Stage 2.0 - Add new feature (exit)

--- app/src/main/java/com/ste/sokoban/Game_Main.java
+++ app/src/main/java/com/ste/sokoban/Game_Main.java
@@ -1,7 +1,11 @@
 package com.ste.sokoban;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Game_Main extends Activity {
 
@@ -9,5 +13,40 @@ public class Game_Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_main);
+    }
+
+    public void onClick(View view) {
+
+        Intent intent = null;
+
+        switch (view.getId()) {
+            case R.id.btnExit:
+                isFinish();
+                break;
+        }
+    }
+
+    public void isFinish() {
+        AlertDialog.Builder alrDiag = new AlertDialog.Builder(this);
+
+        alrDiag.setTitle("Wait!");
+        alrDiag.setMessage("Do u REALLY wanna quit?");
+
+        alrDiag.setPositiveButton("Yep!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Game_Main.this.finish();
+            }
+        });
+
+        alrDiag.setNeutralButton("Nooo..", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // pass
+            }
+        });
+
+        alrDiag.create();
+        alrDiag.show();
     }
 }
